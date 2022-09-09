5311e4802bba69a07a00f2d7ccab00a536d178d9
baikauskaite <baikauskaitev@gmail.com>
2022-09-09 11:19:51 +0300

    Add regex pattern matching

--- src/SpecialTask.java
+++ src/SpecialTask.java
@@ -3,10 +3,12 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SpecialTask {
-
     static final String INPUT_PATH = "data/input.txt";
+    static final String REGEX_PATTERN = "[A-Za-z0-9]*[1-9]\\d+[A-Za-z0-9]*";
 
     public static void main(String[] args) throws Exception {
         File inputFile = new File(INPUT_PATH);
@@ -17,5 +19,15 @@ public class SpecialTask {
         while ((line = bufferedReader.readLine()) != null) {
             inputList.add(line);
         }
+
+        inputList.stream()
+                .filter(SpecialTask::matchesPattern)
+                .forEach(System.out::println);
+    }
+
+    public static boolean matchesPattern(String line) {
+        Pattern pattern = Pattern.compile(REGEX_PATTERN);
+        Matcher matcher = pattern.matcher(line);
+        return matcher.matches();
     }
 }
